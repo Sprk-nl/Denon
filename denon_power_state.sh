05247c64d0291c082bb1ee1069ab9725167cff23
Sprk-nl <gaston.bougie@gmail.com>
2015-12-30 11:07:50 +0100

    Create denon_power_state.sh

---
+++ denon_power_state.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# A file for Domoticz to check if the Denon receiver and a virtual switch have the correct state.
+# Run this script in cron every minute.
+
+switchID=35
+domoticzIP=192.168.0.16
+denonIP=192.168.0.34
+
+  DENONPOWER=`curl -s http://$denonIP/goform/formMainZone_MainZoneXml.xml | grep -oP '(?<=<Power><value>).*(?=</value)'`
+  echo Denon status is $DENONPOWER
+
+  SWITCHSTATUS=`curl -s "http://$domoticzIP:8080/json.htm?type=devices&rid=$switchID" | grep -oP '(?<="Status" : ").*(?=",)'`
+  echo Switch $switch status is $SWITCHSTATUS
+
+
+   if [ "$SWITCHSTATUS" == "Off" ] && [ "$DENONPOWER" == "STANDBY" ]
+    then
+     echo Switchstatus and Receiver are off, Skip sending command to Domoticz;
+   elif [ "$SWITCHSTATUS" == "On" ] && [ "$DENONPOWER" == "ON" ]
+    then
+     echo Switchstatus and Receiver are on, Skip sending command to Domoticz;
+   elif [ "$SWITCHSTATUS" == "Off" ] && [ "$DENONPOWER" == "ON" ]
+    then
+     echo Switchstatus is Off, Denon is ON, updating Switch $switch;
+     curl -s "http://$domoticzIP:8080/json.htm?type=command&param=switchlight&idx=$switchID&switchcmd=On" > /dev/null
+   elif [ "$SWITCHSTATUS" == "On" ] && [ "$DENONPOWER" == "STANDBY" ]
+    then
+     echo Switchstatus is On, Denon is OFF, updating Switch $switch;
+     curl -s "http://$domoticzIP:8080/json.htm?type=command&param=switchlight&idx=$switchID&switchcmd=Off" > /dev/null
+fi
